972846a0e81e1b1c27b3e68f3a65bb045508fdf7
Stephy <507452965@qq.com>
2019-01-23 22:57:54 +0800

    Add FileUpload Class with Spring Boot

--- src/main/java/com/rebel/firstappdemo/controller/FileUploadController.java
+++ src/main/java/com/rebel/firstappdemo/controller/FileUploadController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.awt.*;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
