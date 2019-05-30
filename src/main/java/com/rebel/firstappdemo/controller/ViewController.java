9892ca411ec9d01975401154284b40714bc8e533
Stephy <507452965@qq.com>
2019-05-30 10:21:12 +0800

    Add Thymeleaf Restful Web Service

---
+++ src/main/java/com/rebel/firstappdemo/controller/ViewController.java
@@ -0,0 +1,17 @@
+package com.rebel.firstappdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ViewController {
+    @RequestMapping(value = "/view-products")
+    public String viewProduct(){
+        return "view-products";
+    }
+
+    @RequestMapping(value = "/add-products")
+    public String addProducts(){
+        return "add-products";
+    }
+}
--- src/main/java/com/rebel/firstappdemo/interceptor/ProductServiceInterceptorAppConfig.java
+++ src/main/java/com/rebel/firstappdemo/interceptor/ProductServiceInterceptorAppConfig.java
@@ -1,18 +1,27 @@
 package com.rebel.firstappdemo.interceptor;
 
+import com.rebel.firstappdemo.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 //public class ProductServiceInterceptorAppConfig extends WebMvcConfigurerAdapter {   /*此方法中的WebConfigrtAdapter已经过时，因为是基于spring5
 //在Spring 5.0 中，已经将 WebMvcConfigurerAdapter 抽象类加上 @Deprecated 注解 记为过时。
+
 @Component
 public class ProductServiceInterceptorAppConfig implements WebMvcConfigurer {
+
+/*    @Autowired
+    ProductServiceInterceptor productServiceInterceptor;*/
+
+    private ProductServiceInterceptor productServiceInterceptor;
+
     @Autowired
-    ProductServiceInterceptor productServiceInterceptor;
+    public void setProductServiceInterceptor(ProductServiceInterceptor productServiceInterceptor){
+        this.productServiceInterceptor=productServiceInterceptor;
+    }
 
     @Override
     public  void addInterceptors(InterceptorRegistry registry) {
